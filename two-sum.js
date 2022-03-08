a31aeddc8d8e64a3a39c4173cc5f6c52ff479648
zhengshuang2011 <zhengshuang2011@email.com>
2022-03-08 17:17:24 -0500

    Two-sum.js added

---
+++ two-sum.js
@@ -0,0 +1,27 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+let nums = [3, 3, 2, 4];
+let target = 6;
+//  console.log(nums.indexOf(3));
+//  let newarr = nums.slice(1);
+//  console.log(newarr);
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSum(nums, target));
